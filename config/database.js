d4beeee3e6ef3d2defeacc52e94998324752f904
chetoui hamza <okuninoshi>
2021-04-29 17:46:33 +0100

    remove db secrets

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.71bed.mongodb.net'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-local-library'),
-        username: env('DATABASE_USERNAME', 'strapi-admin'),
-        password: env('DATABASE_PASSWORD', 'strap1-adm1n'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
