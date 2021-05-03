1b56b38882bcaecdd414f54dfb0071a7ed3fc499
chetoui hamza <okuninoshi>
2021-05-03 02:42:20 +0100

    check if book with same isbn in create controller

--- api/book/controllers/book.js
+++ api/book/controllers/book.js
@@ -12,17 +12,42 @@ module.exports = {
 
   async delete(ctx) {
     const { id } = ctx.params;
-      //if book has more then one instance 
-      // we prevent user from deleting the book
-      // and return 406 not acceptable
-      let entity = await strapi.services.book.find({ id });
-      if(entity[0].bookinstances.length > 0) {
-         return ctx.send({
-            message: 'book contain one or more instances'
-        }, 406);
-      }
-      //else delete the book
-      entity = await strapi.services.book.delete({ id });
-      return sanitizeEntity(entity, { model: strapi.models.book });
+    //if book has more then one instance 
+    // we prevent user from deleting the book
+    // and return 406 not acceptable
+    let entity = await strapi.services.book.find({ id });
+    if(entity[0].bookinstances.length > 0) {
+        return ctx.send({
+          message: 'book contain one or more instances'
+      }, 406);
+    }
+    //else delete the book
+    entity = await strapi.services.book.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.book });
+  },
+
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+  // because ISBN is unique we can't
+  //the use can't create two books with the same ISBN
+   async create(ctx) {
+    let entity;
+    const { ISBN } = ctx.request.body
+    entity = await strapi.services.book.findOne({ ISBN });
+    if (entity){
+        return ctx.send({
+          message: 'book alredy existe'
+      }, 406);
+    }
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.book.create(data, { files });
+    } else {
+      entity = await strapi.services.book.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.book });
   },
 };
--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
+        host: env('DATABASE_HOST', 'cluster0.71bed.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        database: env('DATABASE_NAME', 'strapi-local-library'),
+        username: env('DATABASE_USERNAME', 'strapi-admin'),
+        password: env('DATABASE_PASSWORD', 'strap1-adm1n'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
